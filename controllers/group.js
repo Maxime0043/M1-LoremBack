964323d38ab6146149d75c88848976e8dceb1e77
Pinna Maxime <max.pinna@hotmail.fr>
2022-06-28 11:35:28 +0200

    OK : Creation of a route to retrieve all groups from an editor

--- controllers/group.js
+++ controllers/group.js
@@ -15,3 +15,11 @@ exports.get = async function (req, res) {
   const groupId = req.params.id;
   res.status(200).json(await Group.findById(groupId));
 };
+
+/**
+ * Allows you to retrieve all the groups of an editor.
+ */
+exports.getFromEditor = async function (req, res) {
+  const editorId = req.params.id;
+  res.status(200).json(await Group.find({ id_editor: editorId }));
+};
--- routes/groups.js
+++ routes/groups.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const Group = require("../controllers").group;
 const { validGroup } = require("../middlewares/group.middleware");
+const { validEditor } = require("../middlewares/user.middleware");
 const router = express.Router();
 
 router.get("/", Group.getAll);
 router.get("/:id", [validGroup], Group.get);
+router.get("/editor/:id", [validEditor], Group.getFromEditor);
 
 module.exports = router;
