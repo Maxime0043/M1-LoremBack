b9a24874a949b313c521647305bca549d755b9ca
Pinna Maxime <max.pinna@hotmail.fr>
2022-06-28 09:42:40 +0200

    Correction of the listening port

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ require("./database/connection");
 
 const app = require("./app");
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Serveur écoute sur le port 5000.");
 });
