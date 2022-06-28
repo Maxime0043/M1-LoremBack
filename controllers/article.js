a524bc94dc8e770c4527e5bc41b0ad8633a22df1
Léo Bonhême--Morsent <gturyz@gmail.com>
2022-06-28 16:03:34 +0200

    WIP : Add route update by id for article

--- controllers/article.js
+++ controllers/article.js
@@ -1,4 +1,5 @@
 const { Article } = require("../database/models/Article.model");
+const { RequestState } = require("../database/enum");
 const Joi = require("joi");
 
 exports.getAll = async function (req, res) {
@@ -36,4 +37,31 @@ exports.get = async function (req, res) {
   if (!article) return res.status(400).json({ error: "Invalid Article Id !" });
 
   res.status(200).json(article);
+};
+
+exports.update = async function (req, res) {
+  const { id } = req.params;
+  const payload = req.body;
+  const user = req.user;
+  const schema = Joi.object({
+    title: Joi.string().min(3),
+    image: Joi.string().min(2),
+    content: Joi.string().min(50),
+    published: Joi.string().valid(...Object.values(RequestState)),
+  }).min(1);
+
+  const { value: newArticle, error } = schema.validate(payload);
+  if (error) return res.status(400).json({ error: error.details[0].message });
+
+  const oldArticle = await Article.findById(id);
+
+  if (!oldArticle)
+    return res.status(400).json({ error: "Invalid Article Id !" });
+  if (oldArticle.id_author.toString() !== user.id)
+    return res
+      .status(400)
+      .json({ error: "User is not the owner of the article !" });
+
+  const article = await oldArticle.updateOne(newArticle);
+  res.status(200).json(await Article.findById(id));
 };
