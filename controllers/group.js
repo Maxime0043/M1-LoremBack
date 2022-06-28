04bb9ca7e49644490cde47bd527497f5a299290e
Pinna Maxime <max.pinna@hotmail.fr>
2022-06-28 13:47:07 +0200

    OK : Creation of the route to add groups

--- controllers/group.js
+++ controllers/group.js
@@ -1,3 +1,4 @@
+const joi = require("joi");
 // Import Model
 const { Group } = require("../database/models/Group.model");
 
@@ -22,4 +23,35 @@ exports.get = async function (req, res) {
 exports.getFromEditor = async function (req, res) {
   const editorId = req.params.id;
   res.status(200).json(await Group.find({ id_editor: editorId }));
+};
+
+/**
+ * Allows you to create a group without articles.
+ */
+exports.create = async function (req, res) {
+  const editor = req.user;
+  const payload = req.body;
+
+  // Validation
+  const schema = joi.object({
+    title: joi.string().min(3).required(),
+  });
+  const { value, error } = schema.validate(payload);
+
+  // If the fields have been filled in incorrectly
+  if (error) {
+    res.status(400).json({ error: error.details[0].message });
+    return;
+  } else {
+    // We fill in the missing data
+    value.articles = [];
+    value.id_editor = editor.id;
+
+    // Creation of the group
+    let group = new Group(value);
+    group = await group.save();
+
+    // Returns created object
+    res.status(201).json(group);
+  }
 };
--- routes/groups.js
+++ routes/groups.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Group = require("../controllers").group;
+const { authGuard } = require("../middlewares/auth.middleware");
 const { validGroup } = require("../middlewares/group.middleware");
 const { validEditor } = require("../middlewares/user.middleware");
 const router = express.Router();
@@ -7,5 +8,7 @@ const router = express.Router();
 router.get("/", Group.getAll);
 router.get("/:id", [validGroup], Group.get);
 router.get("/editor/:id", [validEditor], Group.getFromEditor);
+
+router.post("/", [authGuard], Group.create);
 
 module.exports = router;
