0bdddd3605419efb1c9d3d6ed78683c7a9a65c79
Léo Bonhême--Morsent <gturyz@gmail.com>
2022-06-28 16:00:45 +0200

    WIP : Add route getAll for article

--- controllers/article.js
+++ controllers/article.js
@@ -1,3 +1,6 @@
-exports.create = function (req, res) {
-  res.json({ message: "Example POST /api/v1/article" });
+const { Article } = require("../database/models/Article.model");
+
+exports.getAll = async function (req, res) {
+  const articles = await Article.find();
+  res.status(200).json(articles);
 };
